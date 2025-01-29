9f7e9a097793cc4ad3b23a0a1ac0e1747bc4d2e9
Sekky61 <misa@majer.cz>
2025-01-29 12:13:12 +0100

    Exports all types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export {CodeSample, type CodeSampleObject} from './CodeSample';
+export type {Token, HoverInfo, CodeRange, CodePosition, HoverContents} from './CodeSample';
 export {CopyButton} from './CodeSample/CopyButton';
 export * from './CodeSampleHelpers';
